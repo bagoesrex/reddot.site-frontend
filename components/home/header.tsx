ade6eaeaebb46b852d86bc08e8fa47315d453307
bagoesrex <bagoesrex@gmail.com>
2025-07-03 20:08:36 +0700

    style: adjust gap in NavigationMenuList layout

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "./navbar"
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-red-700 shadow-md">
+        <header className="sticky top-0 z-50 bg-gray-700 shadow-md">
             <div className="flex flex-row justify-between items-center px-7">
                 <div className="overflow-hidden">
                     <img src="/logo.svg" alt="Logo Website" className="h-12 w-auto" />
