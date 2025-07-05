c7b946f35fcb934847ffa10702600bfa83085569
bagoesrex <bagoesrex@gmail.com>
2025-07-05 20:58:49 +0700

    feat: implement animation to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import Icon from "@/public/ai-icon.webp";
 import Kucink from "@/public/kucink.jpg";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <section id="hero" className="flex flex-col md:flex-row min-h-svh bg-gradient-to-tr from-rose-400 to-white justify-center items-center gap-2 pb-16 pt-[120px]">
         <div className="max-w-xl text-center md:text-left px-5">
-          <h1 className="text-4xl md:text-6xl font-extrabold drop-shadow-md leading-tight">
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="text-4xl md:text-6xl font-extrabold drop-shadow-md leading-tight"
+          >
             Awali Perjalanan AI-mu di <span className="text-red-600">Reddot</span>
-          </h1>
-          <p className="text-md md:text-xl mt-4 text-gray-700">
+          </motion.h1>
+
+          <motion.p
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
+            className="text-md md:text-xl mt-4 text-gray-700"
+          >
             Platform edukasi untuk memahami kecerdasan buatan secara menyeluruh dan mudah diakses semua orang.
-          </p>
+          </motion.p>
         </div>
 
-        <div className="w-[300px] aspect-[1/1]">
+        <motion.div
+          className="w-[300px] aspect-[1/1]"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.7, delay: 0.4, ease: "easeOut" }}
+        >
           <Image
             src={Icon}
             alt="AI Icon"
@@ -24,7 +43,7 @@ export default function Home() {
             className="object-contain"
             priority
           />
-        </div>
+        </motion.div>
       </section>
 
       <section id="about" className="flex flex-col md:flex-col min-h-svh bg-gray-200 items-center">
