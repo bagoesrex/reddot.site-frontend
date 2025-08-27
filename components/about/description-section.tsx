84149bf49fde3a2deab23e91abb5661bf74f206d
bagoesrex <bagoesrex@gmail.com>
2025-08-28 05:51:08 +0700

    style(about): adjust description section layout and styles for mobile

--- components/about/description-section.tsx
+++ components/about/description-section.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function DescriptionSection() {
     return (
-        <section id="description" className="flex flex-col md:flex-col min-h-[26rem] items-center pb-15 pt-15 px-5 max-w-4xl w-full">
-            <div className="flex flex-row w-full items-center">
+        <section id="description" className="flex flex-col md:flex-col min-h-[26rem] items-center pb-15 md:pt-15 px-5 max-w-4xl w-full">
+            <div className="flex flex-col md:flex-row w-full items-center">
                 <div className="flex-1/2 flex justify-center items-center w-full">
                     <div className="relative w-[310px] h-[310px]">
                         <Image
@@ -29,7 +29,7 @@ export default function DescriptionSection() {
                     </div>
                 </div>
                 <div className="flex-1/2 justify-center items-center w-full">
-                    <p className="text-xl leading-9">Reddot Community berasal dari <span className="text-[#D10000] font-semibold">Semarang</span>, dan dibentuk sebagai wadah kolaboratif bagi
+                    <p className="text-md md:text-xl text-center leading-9">Reddot Community berasal dari <span className="text-[#D10000] font-semibold">Semarang</span>, dan dibentuk sebagai wadah kolaboratif bagi
                         siapa saja yang tertarik dengan dunia Artificial Intelligence (AI). Kami percaya bahwa masa depan AI yang inklusif dan
                         bermanfaat dimulai dari komunitas yang aktif belajar, berbagi, dan tumbuh bersama.
                     </p>
